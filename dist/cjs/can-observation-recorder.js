15939cda8b1b00f43ae69ac7d8072dec77dce941
Justin Meyer <justinbmeyer@gmail.com>
2017-10-06 08:09:56 -0500

    0.0.1

---
+++ dist/cjs/can-observation-recorder.js
@@ -0,0 +1,105 @@
+/*can-observation-recorder@0.0.0#can-observation-recorder*/
+var namespace = require('can-namespace');
+var stack = [];
+var observationRecorder = {
+    stack: stack,
+    makeDependenciesRecorder: function () {
+        return {
+            traps: null,
+            keyDependencies: new Map(),
+            valueDependencies: new Set(),
+            ignore: 0
+        };
+    },
+    start: function () {
+        stack.push({
+            traps: null,
+            keyDependencies: new Map(),
+            valueDependencies: new Set(),
+            ignore: 0
+        });
+    },
+    stop: function () {
+        return stack.pop();
+    },
+    add: function (obj, event) {
+        var top = stack[stack.length - 1];
+        if (top && top.ignore === 0) {
+            if (top.traps) {
+                top.traps.push([
+                    obj,
+                    event
+                ]);
+            } else {
+                if (event === undefined) {
+                    top.valueDependencies.add(obj);
+                } else {
+                    var eventSet = top.keyDependencies.get(obj);
+                    if (!eventSet) {
+                        eventSet = new Set();
+                        top.keyDependencies.set(obj, eventSet);
+                    }
+                    eventSet.add(event);
+                }
+            }
+        }
+    },
+    addMany: function (observes) {
+        var top = stack[stack.length - 1];
+        if (top) {
+            if (top.traps) {
+                top.traps.push.apply(top.traps, observes);
+            } else {
+                for (var i = 0, len = observes.length; i < len; i++) {
+                    this.observe(observes[i][0], observes[i][1]);
+                }
+            }
+        }
+    },
+    ignore: function (fn) {
+        return function () {
+            if (stack.length) {
+                var top = stack[stack.length - 1];
+                top.ignore++;
+                var res = fn.apply(this, arguments);
+                top.ignore--;
+                return res;
+            } else {
+                return fn.apply(this, arguments);
+            }
+        };
+    },
+    trap: function () {
+        if (stack.length) {
+            var top = stack[stack.length - 1];
+            var oldTraps = top.traps;
+            var traps = top.traps = [];
+            return function () {
+                top.traps = oldTraps;
+                return traps;
+            };
+        } else {
+            return function () {
+                return [];
+            };
+        }
+    },
+    trapsCount: function () {
+        if (stack.length) {
+            var top = stack[stack.length - 1];
+            return top.traps.length;
+        } else {
+            return 0;
+        }
+    },
+    isRecording: function () {
+        var len = stack.length;
+        var last = len && stack[len - 1];
+        return last && last.ignore === 0 && last;
+    }
+};
+if (namespace.observationRecorder) {
+    throw new Error('You can\'t have two versions of can-observation-recorder, check your dependencies');
+} else {
+    module.exports = namespace.observationRecorder = observationRecorder;
+}
---
+++ dist/global/can-observation-recorder.js
@@ -0,0 +1,247 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval) {
+	// jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name) {
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for (i = 0; i < parts.length; i++) {
+			if (!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val) {
+		var parts = name.split("."),
+			cur = global,
+			i,
+			part,
+			next;
+		for (i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if (!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod) {
+		if (!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, default: true };
+		for (var p in mod) {
+			if (!esProps[p]) return false;
+		}
+		return true;
+	};
+
+	var hasCjsDependencies = function(deps) {
+		return (
+			deps[0] === "require" && deps[1] === "exports" && deps[2] === "module"
+		);
+	};
+
+	var modules =
+		(global.define && global.define.modules) ||
+		(global._define && global._define.modules) ||
+		{};
+	var ourDefine = (global.define = function(moduleName, deps, callback) {
+		var module;
+		if (typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for (i = 0; i < deps.length; i++) {
+			args.push(
+				exports[deps[i]]
+					? get(exports[deps[i]])
+					: modules[deps[i]] || get(deps[i])
+			);
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if (hasCjsDependencies(deps) || (!deps.length && callback.length)) {
+			module = { exports: {} };
+			args[0] = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args[1] = module.exports;
+			args[2] = module;
+		} else if (!args[0] && deps[0] === "exports") {
+			// Babel uses the exports and module object.
+			module = { exports: {} };
+			args[0] = module.exports;
+			if (deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if (!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if (globalExport && !get(globalExport)) {
+			if (useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	});
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function() {
+		// shim for @@global-helpers
+		var noop = function() {};
+		return {
+			get: function() {
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load) {
+				doEval(__load.source, global);
+			}
+		};
+	});
+})(
+	{},
+	typeof self == "object" && self.Object == Object ? self : window,
+	function(__$source__, __$global__) {
+		// jshint ignore:line
+		eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+	}
+);
+
+/*can-observation-recorder@0.0.0#can-observation-recorder*/
+define('can-observation-recorder', [
+    'require',
+    'exports',
+    'module',
+    'can-namespace'
+], function (require, exports, module) {
+    var namespace = require('can-namespace');
+    var stack = [];
+    var observationRecorder = {
+        stack: stack,
+        makeDependenciesRecorder: function () {
+            return {
+                traps: null,
+                keyDependencies: new Map(),
+                valueDependencies: new Set(),
+                ignore: 0
+            };
+        },
+        start: function () {
+            stack.push({
+                traps: null,
+                keyDependencies: new Map(),
+                valueDependencies: new Set(),
+                ignore: 0
+            });
+        },
+        stop: function () {
+            return stack.pop();
+        },
+        add: function (obj, event) {
+            var top = stack[stack.length - 1];
+            if (top && top.ignore === 0) {
+                if (top.traps) {
+                    top.traps.push([
+                        obj,
+                        event
+                    ]);
+                } else {
+                    if (event === undefined) {
+                        top.valueDependencies.add(obj);
+                    } else {
+                        var eventSet = top.keyDependencies.get(obj);
+                        if (!eventSet) {
+                            eventSet = new Set();
+                            top.keyDependencies.set(obj, eventSet);
+                        }
+                        eventSet.add(event);
+                    }
+                }
+            }
+        },
+        addMany: function (observes) {
+            var top = stack[stack.length - 1];
+            if (top) {
+                if (top.traps) {
+                    top.traps.push.apply(top.traps, observes);
+                } else {
+                    for (var i = 0, len = observes.length; i < len; i++) {
+                        this.observe(observes[i][0], observes[i][1]);
+                    }
+                }
+            }
+        },
+        ignore: function (fn) {
+            return function () {
+                if (stack.length) {
+                    var top = stack[stack.length - 1];
+                    top.ignore++;
+                    var res = fn.apply(this, arguments);
+                    top.ignore--;
+                    return res;
+                } else {
+                    return fn.apply(this, arguments);
+                }
+            };
+        },
+        trap: function () {
+            if (stack.length) {
+                var top = stack[stack.length - 1];
+                var oldTraps = top.traps;
+                var traps = top.traps = [];
+                return function () {
+                    top.traps = oldTraps;
+                    return traps;
+                };
+            } else {
+                return function () {
+                    return [];
+                };
+            }
+        },
+        trapsCount: function () {
+            if (stack.length) {
+                var top = stack[stack.length - 1];
+                return top.traps.length;
+            } else {
+                return 0;
+            }
+        },
+        isRecording: function () {
+            var len = stack.length;
+            var last = len && stack[len - 1];
+            return last && last.ignore === 0 && last;
+        }
+    };
+    if (namespace.observationRecorder) {
+        throw new Error('You can\'t have two versions of can-observation-recorder, check your dependencies');
+    } else {
+        module.exports = namespace.observationRecorder = observationRecorder;
+    }
+});
+/*[global-shim-end]*/
+(function(global) { // jshint ignore:line
+	global._define = global.define;
+	global.define = global.define.orig;
+}
+)(typeof self == "object" && self.Object == Object ? self : window);
