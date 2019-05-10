1c5052e6fd9b350dbada2824cebba6fd94fc4dad
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 19:06:48 +0100

    Landscaper: QUnit2 upgrade

--- can-observation-recorder-test.js
+++ can-observation-recorder-test.js
@@ -4,7 +4,7 @@ var canSymbol = require("can-symbol");
 
 QUnit.module('can-observation-recorder');
 
-QUnit.test('basics', function() {
+QUnit.test('basics', function(assert) {
 	var foo = {};
 	var bar = {};
 	observationRecorder.start();
@@ -13,14 +13,14 @@ QUnit.test('basics', function() {
 	observationRecorder.add(bar);
 
 	var dependencies = observationRecorder.stop();
-	QUnit.ok(dependencies.keyDependencies.get(foo), "has foo in key deps");
-	QUnit.ok(dependencies.keyDependencies.get(foo).has("event1"), "has foo event1 ");
-	QUnit.ok(dependencies.valueDependencies.has(bar), "has foo in key deps");
+	assert.ok(dependencies.keyDependencies.get(foo), "has foo in key deps");
+	assert.ok(dependencies.keyDependencies.get(foo).has("event1"), "has foo event1 ");
+	assert.ok(dependencies.valueDependencies.has(bar), "has foo in key deps");
 });
 
 
 
-QUnit.test('start returns the same deps as stop', function() {
+QUnit.test('start returns the same deps as stop', function(assert) {
 	var foo0 = {};
 
 	var dependencies0Start, dependencies0Stop;
@@ -29,22 +29,22 @@ QUnit.test('start returns the same deps as stop', function() {
 	observationRecorder.add(foo0, "event0");
 	dependencies0Stop = observationRecorder.stop();
 
-	QUnit.ok(dependencies0Start.keyDependencies.get(foo0), "dependencies0 has foo0 in key deps");
-	QUnit.equal(dependencies0Start, dependencies0Stop, "dependencies0Start is the same as dependencies0Stop");
+	assert.ok(dependencies0Start.keyDependencies.get(foo0), "dependencies0 has foo0 in key deps");
+	assert.equal(dependencies0Start, dependencies0Stop, "dependencies0Start is the same as dependencies0Stop");
 
 });
 
-QUnit.test("created adds a dependency to the childDependencies of the parent", function() {
+QUnit.test("created adds a dependency to the childDependencies of the parent", function(assert) {
 	var foo = function() {};
 
 	observationRecorder.start();
 	observationRecorder.created(foo);
 	var dependencies = observationRecorder.stop();
 
-	QUnit.ok(dependencies.childDependencies.has(foo), "dependencies has foo in the child deps");
+	assert.ok(dependencies.childDependencies.has(foo), "dependencies has foo in the child deps");
 });
 
-QUnit.test("add peekValue", function(){
+QUnit.test("add peekValue", function(assert) {
 
 	var obs = {};
 	obs[canSymbol.for("can.getValue")] = function(){
@@ -53,11 +53,11 @@ QUnit.test("add peekValue", function(){
 	};
 
 	observationRecorder.start();
-	QUnit.equal( observationRecorder.peekValue(obs), 3);
+	assert.equal( observationRecorder.peekValue(obs), 3);
 	var dependencies0Stop = observationRecorder.stop();
-	QUnit.equal(dependencies0Stop.valueDependencies.size, 0);
+	assert.equal(dependencies0Stop.valueDependencies.size, 0);
 
-	QUnit.equal( observationRecorder.peekValue(obs), 3, "read outside observable");
-	QUnit.equal( observationRecorder.peekValue(0), 0, "read primitive");
-	QUnit.equal( observationRecorder.peekValue(true), true, "read primitive");
+	assert.equal( observationRecorder.peekValue(obs), 3, "read outside observable");
+	assert.equal( observationRecorder.peekValue(0), 0, "read primitive");
+	assert.equal( observationRecorder.peekValue(true), true, "read primitive");
 });
